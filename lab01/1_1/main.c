e0a0b43dde4b18fc45a233a66c97141f5e3a3aeb
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2025-02-26 13:38:43 +0100

    lab01

---
+++ lab01/1_1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Podaj liczbe\n");
+    int a;
+    scanf("%d", &a);
+    printf("Liczba: %d", a);
+    return 0;
+}
---
+++ lab01/1_2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a,b;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    printf("%f", a-b);
+    return 0;
+}
