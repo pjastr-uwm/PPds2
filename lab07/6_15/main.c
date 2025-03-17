a93694aa49026a8ec0980ad0ae57ed3979b18842
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-17 14:23:13 +0100

    6_15

---
+++ lab07/6_15/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int ile5=0, ile2=0, ile1=0;
+    while(n>=5){
+        n -=5;
+        ile5++;
+    }
+    while(n>=2){
+        n -=2;
+        ile2++;
+    }
+
+    printf("%d %d %d\n", ile5, ile2, ile1);
+    return 0;
+}
