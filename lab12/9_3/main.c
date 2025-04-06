d25672aab346e6b3849203385165308d244bd172
Piotr Jastrzębski <114875756+pjastr-uwm@users.noreply.github.com>
2025-04-06 12:29:56 +0200

    Update main.c

--- lab12/9_3/main.c
+++ lab12/9_3/main.c
@@ -6,7 +6,7 @@ int check(int(*f1)(int), int (*f2)(int), int n){
         exit(-1);
     }
     for(int i=0;i<=n;i++){
-        if (f1(i) != f2(i)){
+        if (f1(i) * f2(i) <=0){
             return 0;
         }
     }
